f6594d37029bb06542939774f3c215e3cc1e7857
HaniItani <hani.itani.2@kaust.edu.sa>
2023-06-27 02:49:02 +0300

    update readme

--- camel_chat/model/download_camel_models.py
+++ camel_chat/model/download_camel_models.py
@@ -1,11 +1,12 @@
 import argparse
+import os
 
 from huggingface_hub import snapshot_download
 
 
 def dwonload_camel_models(model_name, download_directory):
     
-    snapshot_download(repo_id=f"camel-ai/{model_name}", local_dir=download_directory, local_dir_use_symlinks=False)
+    snapshot_download(repo_id=f"camel-ai/{model_name}", local_dir=os.path.join(download_directory, model_name), local_dir_use_symlinks=False)
 
 
 if __name__ == "__main__":
